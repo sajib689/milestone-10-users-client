f48fd22cff4a6728975adffdfab5abbde11228e3
sajib689 <jibonraja11@gmail.com>
2024-04-17 13:09:40 +0600

    add client

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+
+import Swal from 'sweetalert2'
+
+function App() {
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const location = form.location.value
+    const email = form.email.value
+    const phone = form.phone.value
+    const user = {name,email, phone,location}
+    fetch('http://localhost:3000/users',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then( data => {
+      if(data) {
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "Your Form Fill Up Success",
+          showConfirmButton: false,
+          timer: 1500
+        });
+        
+      }
+    })
+    .catch(err => console.log(err))
+  }
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Form Fill Up now!</h1>
+            <button to='/' className="py-6 btn">
+              Users
+            </button>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                name="name"
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                name="email"
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Location</span>
+                </label>
+                <input
+                name="location"
+                  type="text"
+                  placeholder="location"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                name="phone"
+                  type="text"
+                  placeholder="Phone Number"
+                  className="input input-bordered"
+                  required
+                />
+                
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
