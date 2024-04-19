4344883abb0bcb26e709418c8a44764b4e1118d1
sajib689 <jibonraja11@gmail.com>
2024-04-19 13:12:45 +0600

    client done

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
-
-import { Link, useNavigate } from 'react-router-dom'
-import Swal from 'sweetalert2'
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function App() {
-  const navigate = useNavigate()
-  const handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    const name = form.name.value
-    const location = form.location.value
-    const email = form.email.value
-    const phone = form.phone.value
-    const user = {name,email, phone,location}
-    fetch('https://milestone-10-users-server-git-main-sajib689s-projects.vercel.app/users',{
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(user)
-    })
-    .then(res => res.json())
-    .then( data => {
-      if(data) {
-        Swal.fire({
-          position: "top-center",
-          icon: "success",
-          title: "Your Form Fill Up Success",
-          showConfirmButton: false,
-          timer: 1500
-        });
-        
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const location = form.location.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const user = { name, email, phone, location };
+    fetch(
+      "https://milestone-10-users-server-git-main-sajib689s-projects.vercel.app/users",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(user),
       }
-      form.reset()
-      navigate('/users')
-    })
-    .catch(err => console.log(err))
-  }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Your Form Fill Up Success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+        form.reset();
+        navigate("/users");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Form Fill Up now!</h1>
-            <Link to='/users' className="mt-3 btn btn-secondary">
+            <Link to="/users" className="mt-3 btn btn-secondary">
               Users List
             </Link>
           </div>
@@ -53,7 +54,7 @@ function App() {
                   <span className="label-text">Name</span>
                 </label>
                 <input
-                name="name"
+                  name="name"
                   type="text"
                   placeholder="name"
                   className="input input-bordered"
@@ -65,7 +66,7 @@ function App() {
                   <span className="label-text">Email</span>
                 </label>
                 <input
-                name="email"
+                  name="email"
                   type="text"
                   placeholder="email"
                   className="input input-bordered"
@@ -77,7 +78,7 @@ function App() {
                   <span className="label-text">Location</span>
                 </label>
                 <input
-                name="location"
+                  name="location"
                   type="text"
                   placeholder="location"
                   className="input input-bordered"
@@ -89,13 +90,12 @@ function App() {
                   <span className="label-text">Password</span>
                 </label>
                 <input
-                name="phone"
+                  name="phone"
                   type="text"
                   placeholder="Phone Number"
                   className="input input-bordered"
                   required
                 />
-                
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Submit</button>
